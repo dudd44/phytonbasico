047d0f330b7e4afc85f82fbbdbcba4db86acf20d
Eduarda de Paula Luciano <luciano.eduarda@escola.pr.gov.br>
2026-02-09 14:13:20 +0000

    aula1

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,10 @@ num2  = int (input("Digite o segundo número"))
 
 soma = num1+num2
 sub = num1-num2
-mult = num1.num2
+mult = num1*num2
 div = num1/num2
 
 print("A soma é: ",soma)
+print("A subtração é: ",sub)
+print("A multiplicação é: ",mult)
+print("A divisão é: ",div)
