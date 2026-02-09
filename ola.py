108d570dee57b42f7392230ab879cc93955138cb
Eduarda de Paula Luciano <luciano.eduarda@escola.pr.gov.br>
2026-02-09 14:11:17 +0000

    aula01

---
+++ ola.py
@@ -0,0 +1,6 @@
+#nome = input ("Digite seu nome: ")
+#print("Olá, ", nome,"Seja bem vindo")
+
+nome = input ("Digite seu nome: ")
+sobrenome = input ("Digite seu sobrenome: ")
+print ("Seu nome é", nome, sobrenome)
